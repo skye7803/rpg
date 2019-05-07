31afb8417e6ec9bf303f8affc8e8593de9b163e2
Skye Guice <skye@guices.net>
2019-05-07 13:14:14 -0700

    starting to rewrite

--- rpg_2.py
+++ rpg_2.py
@@ -1,61 +1,90 @@
 class Loop:
-    def __init__(self, loc):
+    def __init__(self, loc, inv):
         self.loop = True
         self.location = loc
+        self.inventory = inv
+        self.move_command = ''
+
+    def gameOver(self):
+        print('  ____                         ___  ')
+        print(' / ___| __ _ _ __ ___   ___   / _ \__   _____ _ __ ')
+        print('| |  _ / _` | `_ ` _ \ / _ \ | | | \ \ / / _ \  __|')
+        print('| |_| | (_| | | | | | |  __/ | |_| |\ V /  __/ |  ')
+        print(' \____|\__,_|_| |_| |_|\___|  \___/  \_/ \___|_| ')
 
     def mainLoop(self):
         while self.loop:
             print('You are ' + self.location.areas[self.location.currentArea]['text'])
-            self.command = input('Which direction would you like to head? ')
-            if self.command in ['north', 'n']:
-                if 'north' in self.location.areas[self.location.currentArea]:
-                    self.location.currentArea = self.location.areas[self.location.currentArea]['north']
-                elif 'north//' in self.location.areas[self.location.currentArea]:
-                    print('You cannot go that way because of a ' + self.location.areas[self.location.currentArea]['north//'])
-                else:
-                    print('You found a bug!')
-            if self.command in ['east', 'e']:
-                if 'east' in self.location.areas[self.location.currentArea]:
-                    self.location.currentArea = self.location.areas[self.location.currentArea]['east']
-                elif 'east//' in self.location.areas[self.location.currentArea]:
-                    print('You cannot go that way because of a ' + self.location.areas[self.location.currentArea]['east//'])
-                else:
-                    print('You found a bug!')
-            if self.command in ['south', 's']:
-                if 'south' in self.location.areas[self.location.currentArea]:
-                    self.location.currentArea = self.location.areas[self.location.currentArea]['south']
-                elif 'south//' in self.location.areas[self.location.currentArea]:
-                    print('You cannot go that way because of a ' + self.location.areas[self.location.currentArea]['south//'])
-                else:
-                    print('You found a bug!')
-            if self.command in ['west', 'w']:
-                if 'west' in self.location.areas[self.location.currentArea]:
-                    self.location.currentArea = self.location.areas[self.location.currentArea]['west']
-                elif 'west//' in self.location.areas[self.location.currentArea]:
-                    print('You cannot go that way because of a ' + self.location.areas[self.location.currentArea]['west//'])
-                else:
-                    print('You found a bug!')
+            self.move_command = input('Which direction would you like to go? ')
+            self.move('north', 'n', 'north//')
+            self.move('east', 'e', 'east//')
+            self.move('south', 's', 'south//')
+            self.move('west', 'w', 'west//')
+            if self.inventory.hunger > 0:
+                self.inventory.hunger -= 1
+            if self.inventory.hunger == 3:
+                print('Getting hungry...')
+            if self.inventory.hunger == 0:
+                self.inventory.health -= 1
+                print('Starving!')
+            if self.inventory.health == 0:
+                self.gameOver()
 
 
+    def move(self, direction, letter, false_direction):
+        if self.move_command in [direction, letter]:
+            if direction in self.location.areas[self.location.currentArea]:
+                self.location.currentArea = self.location.areas[self.location.currentArea][direction]
+            elif false_direction in self.location.areas[self.location.currentArea]:
+                print(
+                    'You cannot go that way because of a ' +
+                    self.location.areas[self.location.currentArea][false_direction])
+            else:
+                print('You found a bug!')
+
+
+class Inventory:
+    def __init__(self):
+        self.health = 10
+        self.hunger = 10
+        self.contents = {
+
+        }
 
 
 class Location:
     def __init__(self):
-        self.currentArea = 'dungeon'
+        self.currentArea = 'starting_dungeon'
         self.areas = {
-            'dungeon': {
-                'id': 'dungeon',
-                'text': 'in a dungeon',
+            'starting_dungeon': {
+                'text': 'in a damp dungeon',
                 'north//': 'a wall',
                 'east//': 'a wall',
                 'south//': 'a wall',
-                'west//': 'a door',
+                'west': 'starting_hallway',
+            },
+            'starting_hallway': {
+                'text': 'in a poorly lit hallway',
+                'north': 'starting_stairs',
+                'east': 'starting_dungeon',
+                'south//': 'a wall',
+                'west//': 'a wall',
+
+            },
+            'starting_stairs': {
+                'text': 'in a crumbling staircase',
+                'north': 'starting_outside',
+                'east//': 'a wall',
+                'south//': 'starting_hallway',
+                'west//': 'a wall',
             },
         }
 
 
 location = Location()
 
-loop = Loop(location)
+inventory = Inventory()
+
+loop = Loop(location, inventory)
 
 loop.mainLoop()
